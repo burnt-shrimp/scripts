5044497f5f441ca0728a1d70351ee29ab9c944a7
burntShrimp <you@example.com>
2023-01-14 21:24:45 +0700

    fix script

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ sudo apt install -y zsh
 sudo apt install -y git
 sudo apt install -y tree
 sudo apt install -y zip
+sudo apt install -y ripgrep
 curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
@@ -16,12 +17,10 @@ unzip -p /tmp/win32yank.zip win32yank.exe > /tmp/win32yank.exe
 chmod +x /tmp/win32yank.exe
 sudo mv /tmp/win32yank.exe /usr/local/bin/
 
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
-
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
 curl -LJO https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.deb
 sudo apt install ./nvim-linux64.deb -y
+
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
